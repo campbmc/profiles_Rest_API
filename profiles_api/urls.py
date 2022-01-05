1b6f9692e3762db22c468e338c987a1a54228af1
Leanne <leanne54321@hotmail.co.uk>
2022-01-05 11:28:23 +0000

    corrections

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -3,5 +3,5 @@ from django.urls import paths
 from profiles_api import views
 
 urlpatterns = [
-    path('hello-view/', views.HelloApiView.as_view())
+    path('hello-view/', views.HelloApiView.as_view()),
 ]
